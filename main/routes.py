11b41607948ad48a5669f64ffcfda47ff4b85c09
rahulrameshm0 <iamrahulm23@gmail.com>
2025-02-13 21:55:14 +0530

    created deleting and updating the post

--- main/routes.py
+++ main/routes.py
@@ -1,7 +1,7 @@
 import secrets
 from PIL import Image
 import os
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request, abort
 from main import app, db, bcrypt
 from main.forms import RegistrationForm,LoginForm, UpdateAccountForm, PostForm
 from main.models import User, Post
@@ -97,9 +97,38 @@ def new_post():
         db.session.commit()
         flash('Your post has been successfully created!', "success")
         return redirect(url_for("home"))
-    return render_template("create_post.html", title='New Post', form = form)
+    return render_template("create_post.html", title='New Post', form = form, legend="Update Post")
 
 @app.route("/<int:post_id>")
 def post(post_id):
     post = Post.query.get_or_404(post_id)
     return render_template("post.html", title=post.title, post=post)
+
+@app.route("/<int:post_id>/update", methods=["GET","POST"])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash("Your post has been updated", "success")
+        return redirect(url_for('post', post_id=post_id))
+    elif request.method == "GET":
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template("create_post.html", title="Update Post", form=form, legend="Update Post")
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash("Your post has been deleted!", "success")
+    return redirect(url_for('home'))
